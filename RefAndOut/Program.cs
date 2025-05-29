c252658d07c002cadfe4eb951be4c33822c3fe6a
ThanhTaiNguyen04 <taintse183488@fpt.edu.vn>
2025-05-29 21:49:59 +0700

    Add files via upload

---
+++ RefAndOut/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+void ham1(int n)
+{
+    n = 8;
+    Console.WriteLine($"n trong hàm ={n}");
+}
+int n = 5;
+Console.WriteLine($"n trước khi vào hàm ={n}");
+ham1(n);
+Console.WriteLine($"n sau khi vào hàm ={n}");

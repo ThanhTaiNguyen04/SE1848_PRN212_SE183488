32b7f9f3e9c6a6401f91d55acd887f81b713f32b
ThanhTaiNguyen04 <taintse183488@fpt.edu.vn>
2025-05-29 21:48:20 +0700

    Add files via upload

---
+++ QuaraticEquation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+void first_degree_solution(double a, double b)
+{
+    if (a == 0 && b == 0)
+    {
+        //0x+0=0
+        Console.WriteLine("Vô số nghiệm");
+    }
+    else if (a == 0 && b != 0) 
+    {
+        Console.WriteLine("Vô nghiệm");
+    }
+    else
+    {
+        Console.WriteLine("x={0}", -b / a);
+    }
+}
+void quaratic_equation_solution(double a, double b, double c)
+{
+    if(a == 0)
+    {
+        //Phương trình bậc 1: bx+c=0
+        first_degree_solution(b, c);
+    }
+    else
+    {
+        var delta = Math.Pow(b, 2) - 4 * a * c;
+        if (delta < 0)
+            Console.WriteLine("Vô nghiệm");
+        else if (delta == 0)
+        {
+            Console.WriteLine("x1 = x2 = {0}", -b / (2 * a));
+        }
+        else
+        {
+            var x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            var x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            Console.WriteLine("x1 = {0}\nx2 ={1}", x1, x2);
+        }
+    }
+}
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Phương trình bậc 2:");
+Console.Write("Nhập hệ số a:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Nhập hệ số b:");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Nhập hệ số c:");
+double c = double.Parse(Console.ReadLine());
+Console.WriteLine("{0}x^2+{1}x+{2}=0", a, b, c);
+quaratic_equation_solution(a, b, c);
+Console.ReadLine();

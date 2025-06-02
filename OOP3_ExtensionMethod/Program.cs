675dc39b378dfc13ae95cbca7ba8c5b48e7c87d8
Tài Nguyễn <taintse183488@fpt.edu.vn>
2025-06-02 21:01:07 +0700

    Add files via upload

---
+++ OOP3_ExtensionMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using OOP3_ExtensionMethod;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int n1 = 5;
+int n2 = 10;
+Console.WriteLine($"Tổng từ 1 đến {n1} = {n1.TongTu1toiN()}");
+Console.WriteLine($"Tổng từ 1 đến {n2} = {n2.TongTu1toiN()}");
+Console.WriteLine("Tổng từ 1 đến 8 = " + 8.TongTu1toiN());
+
+Console.WriteLine("8 + 7 = " + 8.Cong(7));
+
+int[] M = new int[8];
+M.TaoMang();
+Console.WriteLine("Mảng sau khi tạo ngẫu nhiên:");
+M.XuatMang();
+M.SapXepMangTangDan();
+Console.WriteLine("Mảng sau khi sắp xếp tăng dần:");
+M.XuatMang();
+M.SapXepMangGiamDan();
+Console.WriteLine("Mảng sau khi sắp xếp giảm dần:");
+M.XuatMang();
---
+++ OOP4_Reuse_OOP2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using OOP2;
+using OOP4_Reuse_OOP2;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+FulltimeEmployee fe = new FulltimeEmployee();
+fe.Id = 1;
+fe.Name = "Putin";
+fe.IdCard = "12345";
+fe.Birthday = new DateTime(1952, 5, 7);
+Console.WriteLine(fe);
+Console.WriteLine("=> AGE = " + fe.Tuoi());
+Console.WriteLine("=> Sinh nhật tháng này? " + (fe.SinhNhatThangNay() ? "Có" : "Không"));
